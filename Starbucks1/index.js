0f4487ddc07913abec6d2ce9f7a5b8eccb3451af
Taylortran96 <tranaithy96@gmail.com>
2023-05-03 23:44:28 +0700

    Fixed

--- Starbucks1/index.js
+++ Starbucks1/index.js
@@ -5,3 +5,9 @@ function changeCircleColor(color){
     const circle = document.querySelector('.circle');
     circle.style.background = color;
 }
+function toggleMenu(){
+    let menuToggle = document.querySelector('.toggle');
+    let navigation = document.querySelector('.navigation')
+    menuToggle.classList.toggle('.active')
+    navigation.classList.toggle('.active')
+}
