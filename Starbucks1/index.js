f50d349e860d848ea74d9b532bee23ce1ca636d6
Taylortran96 <tranaithy96@gmail.com>
2023-05-03 20:42:44 +0700

    Done

---
+++ Starbucks1/index.js
@@ -0,0 +1,7 @@
+function imgSlicer(anything){
+    document.querySelector('.starbucks').src = anything;
+}
+function changeCircleColor(color){
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
